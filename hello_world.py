71b87726b01a59a03fd9d54f3c311ee22de7160c
rainspistols <galchenko.maksym@gmail.com>
2025-08-31 14:19:43 -0700

    Add simple_message.py and simple_messages.py with initial message examples

--- hello_world.py
+++ hello_world.py
@@ -9,3 +9,6 @@ print(message)
 
 message = "Hello Python Crash Course world!"
 print(message)
+
+message = "Hello Python Crash Course reader!"
+print(mesage)
---
+++ simple_messages.py
@@ -0,0 +1,3 @@
+message = "This is the messages"
+message = "This is a new messages"
+print(message)
