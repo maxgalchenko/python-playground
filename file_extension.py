90b0c94f46b10561d315a905e5e58bcdfeee27b7
rainspistols <galchenko.maksym@gmail.com>
2025-09-04 16:08:07 -0700

    Refactor file_extension.py to improve formatting and add price calculation logic

--- file_extension.py
+++ file_extension.py
@@ -1,4 +1,15 @@
-filename = 'python_notes.txt'
+filename = "python_notes.txt"
 print(filename)
 
 print(filename.removesuffix(".txt"))
+
+user_input = int(input())
+cost = 0
+if user_input <= 12:
+    price = 8
+elif user_input <= 64:
+    price = 12
+else:
+    price = 10
+
+print(f"Price is ${price}")
