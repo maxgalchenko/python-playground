97dc495175085c9544846e78e23be82681d532be
rainspistols <galchenko.maksym@gmail.com>
2025-09-10 15:05:13 -0700

    Add book catalog CSV and update test script with new examples
    - Created a new CSV file 'book_catalog_10.csv' containing a list of classic books with their authors and publication years.
    - Modified 'test.py' to include additional examples: clearing a list, using the dictionary get method with a default value, and improved indentation in the shopping cart print loop.

--- test.py
+++ test.py
@@ -7,4 +7,15 @@ shopping_cart.append("milk")
 
 print("Shopping Cart:")
 for item in shopping_cart:
-  print(item)
+    print(item)
+
+
+l = [1, 2, 3, 4, 5]
+l.clear()
+print(l)
+
+diction = {12: 123}
+
+print(diction.get(123321321, 123))
+
+
