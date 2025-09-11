31f9e69d02b85c34fe643bef78edd8e65f9cd304
rainspistols <galchenko.maksym@gmail.com>
2025-09-11 10:48:09 -0700

    Implement quicksort function and add test case for sorting a list of numbers

--- menus.py
+++ menus.py
@@ -1,2 +1,18 @@
 def display_name():
     return 3
+
+
+def quicksort(cards):
+    if len(cards) < 2:
+        return cards  # Base case: Already sorted if 0 or 1 element
+    else:
+        pivot = cards[0]  # Choose first card as pivot
+        less = [i for i in cards[1:] if i <= pivot]
+
+        greater = [i for i in cards[1:] if i > pivot]
+        print(greater, quicksort(greater))
+
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
+print(quicksort([1, 2, 5, 8, 91, 2, 34, 6, 324, 324, 6, 1, 5, 67]))
